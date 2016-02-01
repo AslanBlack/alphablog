a07ab6cd9331305636d3c2eb43af7c4e8213e327
aslanblack <aslanblack@gmx.com>
2016-02-01 10:46:18 +0000

    Style errors ans messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Your article was successfully created"
+      flash[:success] = "Your article was successfully created"
       redirect_to article_path(@article)
     else 
       render 'new' # or :new(both are correct)
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update
       
       if @article.update(article_params)
-        flash[:notice] = "Your article was successfully updated"
+        flash[:success] = "Your article was successfully updated"
         redirect_to article_path(@article)
       else
         render 'edit'
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy 
     
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:warning] = "Article was successfully deleted"
     redirect_to articles_path
   end 
   
